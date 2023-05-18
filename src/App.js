56a787f8038edd05dc70e004ce4f8987c3b813f5
Deepanshu-nainwal <126051377+Deepanshu-nainwal@users.noreply.github.com>
2023-05-18 17:44:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Data from "./Components/LawyerData";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
+}
+
+export default App;
